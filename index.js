fb0ecc647544b5d4f5dd73b1bc6a0f5c8ef31561
Ricardo Jaimes <rickyjaimes@hotmail.com>
2016-05-09 20:24:24 -0400

    added display on input 1 and click

---
+++ index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+var button = $('#button-1');
+button.click(function(){
+	// alert('clicked');
+var value = $('#field-1').val();
+button.click(function(){	
+	$('#display').append("<li>" + value +"</li>");
+
+
+
+});
+
+
+});
+
+
